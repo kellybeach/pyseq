1a7360736096b969fbb31ca183d2f212596a0f32
Kelly Brower <kbrower77@gmail.com>
2018-06-04 18:14:47 -0700

    Update quar.py

--- quar.py
+++ quar.py
@@ -4,10 +4,11 @@
 #went back to Fib code and finished in about ten minutes
 import dis
 def quar(n):
-	a, b  =  0, 1
+	a, b  =  0, 1 #we explicitly define the first two inputs
 	while a < n:
 		print(a, end=' ') #prints all a vaues until loop terminates at input n
-		a, b = a + b, a//b + 1 
+		a, b = a + b, a//b + 1 #the function is defined to replace a, b after first adding the terms, then taking
+		#the integer floor of the quotient a/b and adding 1, then storing these two new values into a, b and repeating
 	print()
 quar(1000)
 dis.dis(quar) # i added dis in an attempt to understand what was going on at the machine level to understand the loop
